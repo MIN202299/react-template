5cfa5523b979197d1230ed63e5ee6b6fe527376e
MIN202299 <min202299@gmail.com>
2023-09-27 14:05:54 +0800

    chore: update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,13 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/prefer-ts-expect-error': 'off',
     'react-hooks/exhaustive-deps': 'off',
-
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-newline': 'error',
+    'react/jsx-first-prop-new-line': 'error',
+    'react/jsx-max-props-per-line': 'error',
+    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+    'react/jsx-closing-bracket-location': [1, 'after-props'],
+    'unused-imports/no-unused-imports': 'off',
   },
   ignorePatterns: [
     '*.svg',
